ffbb3393c1896f0b2dbf7ff1f1ddc989e165d6c7
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2023-07-30 14:39:50 +0200

    Update SetVisibility to also alter active state. Change usage accordingly

--- Assets/Scripts/VolumeGenerationManager.cs
+++ Assets/Scripts/VolumeGenerationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Classes;
@@ -23,10 +24,12 @@ public class VolumeGenerationManager : MonoBehaviour
     /// 2D slice views (on Unity quad mesh) that shows current activeSlice
     /// </summary>
     public SliceView[] sliceViews;
+
     /// <summary>
     /// List of objects that get deleted when a new level is loaded
     /// </summary>
     private List<GameObject> temporaryObjects = new List<GameObject>();
+
     #endregion
 
     #region Initiation
@@ -37,10 +40,9 @@ public class VolumeGenerationManager : MonoBehaviour
         foreach (Transform trans in obj.GetComponentsInChildren<Transform>(true))
         {
             trans.gameObject.layer = visible ? 0 : 3;
-
-            //TODO: Later change to variation of:
-            //trans.gameObject.SetActive(visible);
         }
+
+        obj.gameObject.SetActive(visible);
     }
 
     #endregion
@@ -71,29 +73,33 @@ public class VolumeGenerationManager : MonoBehaviour
             {
                 mKitVolumeVisibleObjects.Add(mKitVolume.GetChild(i));
             }
+
             //TODO: Needs to be deleted on Level change!
-            Transform compareAnchorVisibleVolume = Instantiate(new GameObject("mKitVolumeVisibleObjects"), compareVolumeGrabBox).transform;
+            Transform compareAnchorVisibleVolume =
+                Instantiate(new GameObject("mKitVolumeVisibleObjects"), compareVolumeGrabBox).transform;
             temporaryObjects.Add(compareAnchorVisibleVolume.gameObject);
             foreach (var mKitVolumeVisibleObject in mKitVolumeVisibleObjects)
             {
                 mKitVolumeVisibleObject.SetParent(compareAnchorVisibleVolume);
-                mKitVolumeVisibleObject.Translate(compareAnchor.GetChild(0).position- mKitVolume.position);
+                mKitVolumeVisibleObject.Translate(compareAnchor.GetChild(0).position - mKitVolume.position);
             }
-            compareAnchorVisibleVolume.localRotation= compareAnchor.rotation;
+
+            compareAnchorVisibleVolume.localRotation = compareAnchor.rotation;
+            SetVisibility(compareAnchor.GetChild(0), true);
             //mKitVolume.SetParent(compareVolumeGrabBox);
         }
-        
+
         if (currentLevel.levelType.compareObject == ObjectType.Slice)
         {
-            yield return GetStillDefaultSlice(winningAnswerID, answerAnchors[winningAnswerID],
-                compareAnchor.GetComponentInChildren<RawImage>());
-            SetVisibility(compareAnchor, true);
+            yield return GetStillDefaultSlice(winningAnswerID, answerAnchors[winningAnswerID].GetChild(1),
+                compareAnchor.GetChild(1));
         }
     }
-    private void ResetComponents(Transform[]answerAnchors, Transform compareAnchor)
+
+    private void ResetComponents(Transform[] answerAnchors, Transform compareAnchor)
     {
         //Resets all grabbable boxes to their respective anchor
-        Transform[] anchors = {answerAnchors[0],answerAnchors[1],answerAnchors[2],answerAnchors[3],compareAnchor};
+        Transform[] anchors = {answerAnchors[0], answerAnchors[1], answerAnchors[2], answerAnchors[3], compareAnchor};
         foreach (var anchor in anchors)
         {
             //TODO: Will fail if object is currently grabbed, need to fix!
@@ -101,12 +107,12 @@ public class VolumeGenerationManager : MonoBehaviour
             Transform sliceAnchor = anchor.GetChild(1);
             Transform volumeBoxGrabbable = volumeAnchor.GetChild(0);
             Transform sliceBoxGrabbable = sliceAnchor.GetChild(0);
-            SetVisibility(anchor, false);
-            volumeBoxGrabbable.position = volumeAnchor.position;
-            volumeBoxGrabbable.rotation = volumeAnchor.rotation;
-            sliceBoxGrabbable.position = sliceAnchor.position;
-            sliceBoxGrabbable.rotation = sliceAnchor.rotation;
+            SetVisibility(volumeAnchor, false);
+            SetVisibility(sliceAnchor, false);
+            volumeBoxGrabbable.SetPositionAndRotation(volumeAnchor.position, volumeAnchor.rotation);
+            sliceBoxGrabbable.SetPositionAndRotation(sliceAnchor.position, sliceAnchor.rotation);
         }
+
         //Delete temporary Objects
         foreach (var temporaryObject in temporaryObjects)
         {
@@ -178,49 +184,52 @@ public class VolumeGenerationManager : MonoBehaviour
         v.SliceMaskingTexture = AppConfig.assets.GetScannerMask(scannerType);
         v.UseSliceMasking = scannerType != UltrasoundScannerTypeEnum.LINEAR;
         v.UltrasoundScannerType = scannerType;
+        v.Threshold = 0.001f;
         VolumeManager.Instance.UseMaterialConfigVisualization(v, visualization);
         UltrasoundSimulation.Instance.Init(v);
         Transform volumeAnchor = answerAnchor.GetChild(0);
         v.VolumeProxy.position = volumeAnchor.position; // set volume position
+        SetVisibility(volumeAnchor, true); //Sets all elements of volumeanchor to visible
+        //Only works the first time as it will be disabled on further level
         GameObject.Find("mKitVolume #" + index + " (ArtificialVolume.vm2)").transform
             .SetParent(volumeAnchor.GetChild(0)); //set volumeAnchor's grabbable box as parent of volume
-        SetVisibility(volumeAnchor, true); //Sets all elements of volumeanchor to visible
-        v.Threshold = 0.001f;
     }
 
     /// <summary>
     /// Generates a texture from volume with id of volumeId in default position (centered, straight from the top directed at the bottom) and assigns it to the RawImage targetRawImage
     /// </summary>
-    internal IEnumerator GetStillDefaultSlice(int volumeId, Transform answerAnchor, RawImage targetRawImage)
+    internal IEnumerator GetStillDefaultSlice(int volumeId, Transform sourceSliceAnchor, Transform targetSliceAnchor)
     {
         Transform sliceAnchorTransform = sliceCopyTransform.parent.GetChild(3).transform;
-        foreach (var sliceView in sliceViews)
+        foreach (var sliceView in sliceViews
+        ) //temporarily sets volumes in multiVolume texture to 0 so nothing will get rendered
         {
             sliceView.GetComponent<MeshRenderer>().material
                 .SetFloat("texCount",
-                    0); //temporarily sets volumes in multiVolume texture to 0 so nothing will get rendered
+                    0);
         }
 
         sliceCopyTransform.gameObject.layer = 3; //Make slice temporarily invisible so 
         Vector3 defaultPosition = sliceAnchorTransform.localPosition;
         Quaternion defaultRotation = sliceAnchorTransform.localRotation;
-        yield return sliceAnchorTransform.position = answerAnchor.GetChild(0).position;
-        yield return sliceAnchorTransform.rotation = Quaternion.identity;
-        yield return targetRawImage.texture =
+        sliceAnchorTransform.SetPositionAndRotation(sourceSliceAnchor.position, Quaternion.identity);
+        SetVisibility(targetSliceAnchor,
+            true); //Needs to be done before GetSliceCamCapture, as it will not work without being active
+        yield return targetSliceAnchor.GetComponentInChildren<RawImage>().texture =
             VolumeManager.Instance
                 .GetSliceCamCapture(Volume.Volumes[volumeId]); //Adds still shot of volume of volumeID to stillView
-        yield return sliceAnchorTransform.localPosition = defaultPosition;
-        yield return sliceAnchorTransform.localRotation = defaultRotation;
-        sliceCopyTransform.gameObject.layer = 0;
-        foreach (var sliceView in sliceViews)
+        sliceAnchorTransform.localPosition = defaultPosition;
+        yield return
+            sliceAnchorTransform.localRotation =
+                defaultRotation; //needs yield return or else it will be executed after the foreach
+        foreach (var sliceView in sliceViews
+        ) //sets volumes in multiVolume texture back so everything will get rendered again
         {
             sliceView.GetComponent<MeshRenderer>().material
                 .SetFloat("texCount",
                     Volume.Volumes
-                        .Count); //temporarily sets volumes in multiVolume texture to 0 so nothing will get rendered
+                        .Count);
         }
-
-        //return null;
     }
 
     #endregion

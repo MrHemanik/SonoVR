823eb227387e5b4edbd899b163a17b6572f504cc
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2023-07-30 15:24:09 +0200

    Add InteractionInformation to grabbables to identify their corresponding volume easier

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,27 +5,18 @@ using mKit;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class InputManager : MonoBehaviour
 {
     public GameManager gm;
-    
+
     public void OnLeftTrigger(InputAction.CallbackContext context)
     {
         if (!context.started) return; //Only work when initial click
-
-        foreach (var xrSelectInteractable in GameObject.Find("Left Controller").GetComponent<XRDirectInteractor>().interactablesSelected)
-        {
-            Debug.Log("In Hand: "+xrSelectInteractable.transform.name);
-            for (int i = 0; i < Volume.Volumes.Count; i++)
-            {
-                if(xrSelectInteractable.transform.GetChild(1).name.Contains(""+i))
-                {
-                    gm.CheckAnswer(i);
-                    break;
-                }
-            }
-        }
-
+        int answerId = GameObject.Find("Left Controller").GetComponent<XRDirectInteractor>().interactablesSelected[0]
+            .transform.GetComponent<InteractableInformation>().answerId;
+        if (answerId == 0) return;
+        gm.CheckAnswer(answerId-1);
     }
 }

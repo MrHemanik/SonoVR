d7cb67428cb028f62e967f755caadb9da1bc2a43
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2023-08-04 15:52:03 +0200

    Add popup to inform that you need to hold an answer to submit it via left trigger. Change some materials

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -6,20 +8,20 @@ public class InputManager : MonoBehaviour
 {
     private GameManager gm;
     private XRDirectInteractor leftController;
+    public GameObject popupCanvas;
 
     private void Start()
     {
         gm = FindObjectOfType<GameManager>();
         gm.initLevelEvent.AddListener(ActivateObjectPickUp);
         gm.endLevelEvent.AddListener(DeactivateObjectPickUp);
-        
+
         leftController = GameObject.Find("Left Controller").GetComponent<XRDirectInteractor>();
-        
     }
 
     public void OnLeftTrigger(InputAction.CallbackContext context)
     {
-        if (!context.started || gm.ActiveRound==false) return; //Only work when initial click & round active
+        if (!context.started || gm.ActiveRound == false) return; //Only work when initial click & round active
         if (leftController.hasSelection)
         {
             int answerId = leftController.interactablesSelected[0]
@@ -29,14 +31,25 @@ public class InputManager : MonoBehaviour
         }
         else
         {
-            //Push screen info that you cant
+            //Only start coroutine if no other is running
+            if (!popupCanvas.activeSelf) StartCoroutine(ShowPopupAndHidePopupAfterDelay(popupCanvas, 5.0f));
+        }
+
+        IEnumerator ShowPopupAndHidePopupAfterDelay(GameObject popupCanvas, float delay)
+        {
+            popupCanvas.SetActive(true);
+            yield return new WaitForSeconds(delay);
+            popupCanvas.SetActive(false);
         }
     }
 
+    //EventListener
     private void ActivateObjectPickUp()
     {
         leftController.allowSelect = true;
-    }private void DeactivateObjectPickUp()
+    }
+
+    private void DeactivateObjectPickUp()
     {
         leftController.allowSelect = false; //If there is an object currently grabbed it will cancel it.
     }

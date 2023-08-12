e2612b6c9d37fa221e7a9b66c399400583229f3f
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2023-08-12 14:12:53 +0200

    Fix bug where error would occur when clicking left trigger while grabbing table handle

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -23,8 +23,10 @@ public class InputManager : MonoBehaviour
         if (!context.started || gm.ActiveRound == false) return; //Only work when initial click & round active
         if (leftController.hasSelection)
         {
-            int answerId = leftController.interactablesSelected[0] //TODO: check if it crashes when left trigger while tableheightchange
-                .transform.GetComponent<InteractableInformation>().answerId;
+
+            var infoScript = leftController.interactablesSelected[0].transform.GetComponent<InteractableInformation>();
+            if (infoScript == null) return;
+            int answerId = infoScript.answerId;
             if (answerId == 0) return;
             leftController.allowSelect = false; //Will cancel the pickup, returning the answer to their anchor
             popupCanvas.SetActive(false);
